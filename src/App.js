749875fb6bb58ccb54a996437fd74b61fb59e08c
Benson Toh <benson@eatigo.com>
2020-05-29 23:30:13 +0800

    remove git add comamnd from lint-staged

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
+      <div>test 123</div>
       <span>{process.env.NODE_ENV}</span>
       <span>{process.env.BUILD_ENV}</span>
     </div>
